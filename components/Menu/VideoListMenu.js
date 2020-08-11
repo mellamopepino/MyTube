f547146360bc8f61f0e031e6b3164e1e82548b4b
mellamopepino <delfinagarciav@gmail.com>
2020-08-11 20:05:28 -0300

    Intregrar lista de videos con el back
    La pantalla de videos de cada lista principal ahora está integrada
    a la api de listas, trayendose los videos cuando se entra a la lista.
    También tiene pull to refresh y scroll infinito como en el caso
    de los videos de Home.

--- components/Menu/VideoListMenu.js
+++ components/Menu/VideoListMenu.js
@@ -1,19 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import {
   StyleSheet,
 } from 'react-native';
-
-import { homeSuccessWithMore } from '../../mocks'
 
 import VideoList from '../VideoList'
 
 const VideoListMenu = (props) => {
   const { route } = props
-  const [ videos, setVideos ] = useState(homeSuccessWithMore.items)
+  const [ data, setData ] = useState(null)
+  const [offset, setOffset] = useState(0);
+  const [ refreshing, setRefreshing ] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`http://api-editoriales.clarin.com/api/mobile/v2/oletv/lists/${route.params.id}?${offset}&limit=3`)
+
+      !data ?
+        setData(res.data) :
+        setData((data) => {
+          return {
+            ...data,
+            items: [...data.items, ...res.data.items]
+          }
+        })
+    }
+
+    fetchData()
+  }, [offset])
+
+  const fetchNextPage = async () => {
+    if(data.moreItems) {
+      setOffset(offset+1)
+    }
+  }
+
+  const refreshData = async () => {
+    const res = await axios.get(`http://api-editoriales.clarin.com/api/mobile/v2/oletv/home?offset=0&limit=3`)
+
+    setData(res.data)
+  }
+
+  const handleRefresh = async () => {
+    setRefreshing(true)
+    await refreshData()
+    setRefreshing(false)
+  }
 
   return (
     <VideoList
-      videos={videos}
+      videos={data?.items}
+      onEndReached={fetchNextPage}
+      onRefresh={handleRefresh}
+      refreshing={refreshing}
     />
   )
 }
